2044948ec8eaa063b682f5cbe8e259a75ef610a1
Dmitry Bozhenok <dmitry.bozhenok@gmail.com>
2023-05-19 01:20:42 +0100

    Add project files.

---
+++ ShoppingList.Api/Models/ShoppingListContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ShoppingList.Shared;
+
+namespace ShoppingList.Api.Models
+{
+    public class ShoppingListContext : DbContext
+    {
+        public ShoppingListContext(DbContextOptions<ShoppingListContext> options)
+        : base(options)
+        {
+        }
+
+        public DbSet<Item> ShoppingListItems { get; set; } = null!;
+    }
+}
---
+++ ShoppingList.Shared/Item.cs
@@ -0,0 +1,9 @@
+﻿namespace ShoppingList.Shared
+{
+    public class Item
+    {
+        public int Id { get; set; }
+        public string? Title { get; set; }
+        public bool IsComplete { get; set; }
+    }
+}
