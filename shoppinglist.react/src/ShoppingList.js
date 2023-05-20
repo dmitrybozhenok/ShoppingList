cafe1397ad533bdd5b0a5d81746b8502a14557aa
Dmitry Bozhenok <dmitry.bozhenok@gmail.com>
2023-05-20 23:33:43 +0100

    Changed list of items to use material UI components

--- shoppinglist.react/src/ShoppingList.js
+++ shoppinglist.react/src/ShoppingList.js
@@ -1,5 +1,13 @@
-import React,{Component} from 'react';
+import React,{Component, useEffect, useState} from 'react';
 import {variables} from "./Variables.js";
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import Checkbox from '@mui/material/Checkbox';
+import IconButton from '@mui/material/IconButton';
+import DeleteIcon from '@mui/icons-material/Delete';
 
 export class ShoppingList extends Component{
     constructor(props){
@@ -25,26 +33,51 @@ export class ShoppingList extends Component{
     render(){
         const {
             items
-        }=this.state;
+        } = this.state;
 
+        const handleCompleteClick = (event, id) => {
+            let items = [...this.state.items];
+            let item = items.find(item => {
+                return item.id === id;
+            });
+            item.isComplete = !item.isComplete;
+            items.splice(items.indexOf(item), 1, item);
+            this.setState({ items });
+          }
+        
         return(
-            <div>
-                <h3>
-                    Shopping List
-                </h3>
-                <ol class="list-group">
-                    {items.map(i=>
-                        <li class="list-group-item">
-                            {i.title}
-                            {/* <button type='button' className='btn btn-light mr-1'>
-                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
-                                    <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5Zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5Zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6Z"/>
-                                    <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1ZM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118ZM2.5 3h11V2h-11v1Z"/>
-                                </svg>
-                            </button>                                     */}
-                        </li>                        
+            <div style={{ display: 'flex', justifyContent: 'center' }}>
+    
+                <List sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }} alignItems="center">
+                    {items.map(i =>
+                        <ListItem
+                            key={i.title}
+                            secondaryAction={
+                                <IconButton edge="end" aria-label="delete">
+                                    <DeleteIcon />
+                                </IconButton>
+                            }
+                            style={{ textDecoration : i.isComplete ? 'line-through' : 'none' }} 
+                            disablePadding>
+                            <ListItemButton role={undefined} onClick={(event)=>handleCompleteClick(event, i.id)} dense>
+                                <ListItemIcon>
+                                    <Checkbox
+                                        edge="start"
+                                        checked={items.find(item => {
+                                            return item.id === i.id;
+                                        }).isComplete !== false}
+                                        tabIndex={-1}
+                                        disableRipple
+                                        inputProps={{ 'aria-labelledby': i.id }}
+                                    />
+                                </ListItemIcon>
+                                <ListItemText id={i.id} primary={i.title} />
+                            </ListItemButton>
+                        </ListItem>
+
                     )}
-                </ol>
+                </List>
+                
             </div>
         )
     }
