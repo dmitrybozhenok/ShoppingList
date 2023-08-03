ac21fabc0981d9cd94dcb7e9fca567dd4e87e252
Dmitry Bozhenok <dmitry.bozhenok@gmail.com>
2023-08-03 15:15:07 +0100

    Added proper api url

--- shoppinglist.react/src/Variables.js
+++ shoppinglist.react/src/Variables.js
@@ -1,3 +1,4 @@
 export const variables={
-    API_URL:"https://localhost:7038/api/items/"
+    // API_URL:"https://localhost:7038/api/items/"
+    API_URL:"https://shoppinglist-webapi.azurewebsites.net/api/items/"
 }
